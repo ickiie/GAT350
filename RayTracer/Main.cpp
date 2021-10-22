eb68867c96cf271c7fe46c0f1551c4172852891f
ickiie <nicolasries8@gmail.com>
2021-10-22 09:53:10 -0600

    starting raytracer

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -3,6 +3,7 @@
 #include "Image.h"
 #include "PostProcess.h"
 #include "ColorBuffer.h"
+#include "Tracer.h"
 
 #include <iostream>
 #include <SDL.h>
@@ -17,9 +18,7 @@ int main(int, char**)
 
 	std::unique_ptr<Framebuffer> framebuffer = std::make_unique<Framebuffer>(renderer.get(), renderer->width, renderer->height);
 
-	std::unique_ptr<Image> image = std::make_unique<Image>();
-	image->Load("../resources/sus.bmp", 255);
-	image->Flip();
+	Tracer tracer;
 
 	bool quit = false;
 	SDL_Event event;
@@ -33,85 +32,9 @@ int main(int, char**)
 			break;
 		}
 
-		framebuffer->Clear({0, 0, 0, 0});
-		/*for (int i = 0; i < 100; i++) {
-
-			framebuffer->DrawPoint(rand() % framebuffer->width, rand() % framebuffer->height, { 255, 255, 255, 255 });
-		}
-		*/
-		for (int i = 0; i < 15; i++)
-		{
-			framebuffer->DrawRect(rand() % framebuffer->colorBuffer.width, rand() % framebuffer->colorBuffer.height, 50, 50, { (uint8_t)((rand() % 2) * 255), (uint8_t)((rand() % 2) * 255), (uint8_t)((rand() % 2) * 255), (uint8_t)(rand() % 255) });
-		}
-		/*
-		for (int i = 0; i < 10; i++)
-		{
-			framebuffer->DrawLine(framebuffer->width / 2, framebuffer->height / 2, rand() % framebuffer->width, rand() % framebuffer->height, { 255, 0, 255, 255 });
-		}*/
-
-		//My tests
-		//framebuffer->DrawSimpleCurve(200, 200, 300, 100, 5, { 255, 255, 255, 255 });
-		//framebuffer->DrawQuadraticCurve(200, 200, 300, 100, 400, 200, 35, { 255, 255, 0, 255 });
-		//framebuffer->DrawCubicCurve(200, 200, 300, 100, 400, 200, 300, 200, 35, {255, 0, 255, 255});
-		//framebuffer->DrawCircle(400, 400, 50, { 0, 255, 255, 255 });
-		//framebuffer->DrawTriangle(100, 100, 200, 200, 100, 200, { 0, 125, 125, 255 });
+		framebuffer->Clear({50, 50, 50, 0});
 		
-		
-
-		/*for (int i = 0; i < 100; i++)
-		{
-			framebuffer->DrawPoint(rand() % framebuffer->width, rand() % framebuffer->height, { 0, 255, 0, 0 });
-		}
-		for (int i = 0; i < 20; i++)
-		{
-			framebuffer->DrawRect(rand() % framebuffer->width, rand() % framebuffer->height, 20, 20, { 0, 0, 255, 0 });
-		}
-		for (int i = 0; i < 20; i++)
-		{
-			framebuffer->DrawLine(framebuffer->width >> 1, framebuffer->height >> 1, rand() % framebuffer->width, rand() % framebuffer->height, { 255, 255, 255, 0 });
-		}
-
-
-
-		for (int i = 0; i < 10; i++)
-		{
-			framebuffer->DrawQuadraticCurve(
-				rand() % framebuffer->width, rand() % framebuffer->height,
-				rand() % framebuffer->width, rand() % framebuffer->height,
-				rand() % framebuffer->width, rand() % framebuffer->height, 30, { 255, 255, 0, 255 });
-		}
-		for (int i = 0; i < 10; i++)
-		{
-			framebuffer->DrawCubicCurve(
-				rand() % framebuffer->width, rand() % framebuffer->height,
-				rand() % framebuffer->width, rand() % framebuffer->height,
-				rand() % framebuffer->width, rand() % framebuffer->height,
-				rand() % framebuffer->width, rand() % framebuffer->height,
-				30, { 0, 255, 255, 255 });
-		}*/
-
-		framebuffer->DrawImage(300, 50, image.get());
-
-		std::unique_ptr<Image> image1 = std::make_unique<Image>(*image.get());
-		PostProcess::BoxBlur(image1->colorBuffer);
-		framebuffer->DrawImage(0, 300, image1.get());
-
-		std::unique_ptr<Image> image2 = std::make_unique<Image>(*image.get());
-		PostProcess::GaussianBlur(image2->colorBuffer);
-		framebuffer->DrawImage(200, 300, image2.get());
-
-		std::unique_ptr<Image> image3 = std::make_unique<Image>(*image.get());
-		PostProcess::Sharpen(image3->colorBuffer);
-		framebuffer->DrawImage(400, 300, image3.get());
-
-		std::unique_ptr<Image> image4 = std::make_unique<Image>(*image.get());
-		PostProcess::Monochrome(image4->colorBuffer);
-		PostProcess::Edge(image4->colorBuffer, 0);
-		framebuffer->DrawImage(600, 300, image4.get());
-
-
-		//PostProcess::Invert(framebuffer->colorBuffer);
-		//PostProcess::Noise(framebuffer->colorBuffer, 25);
+		tracer.Trace(framebuffer->colorBuffer);
 
 		framebuffer->Update();
 
---
+++ RayTracer/Tracer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Types.h"
+#include "ColorBuffer.h"
+
+class Tracer {
+
+public:
+	void Trace(const ColorBuffer& colorBuffer);
+
+
+};
